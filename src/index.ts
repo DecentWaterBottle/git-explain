720fda2bc6fd91ee580d6497ddc5a41c31229051
Jack Carter <jack.carter334@gmail.com>
2026-05-02 13:56:57 +0200

    Add file difference extraction

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello");
+import { execSync } from 'child_process'
+
+export function getChangedFiles(hash1: string, hash2: string, repoPath: string): string[] {
+    const output = execSync(`git diff --name-only ${hash1} ${hash2}`, { cwd: repoPath }).toString();
+    return output.trim().split("\n");
+}
+
+export function getFileContent(fileName: string, hash1: string, repoPath: string): string {
+    return execSync(`git show ${hash1}:${fileName}`, { cwd: repoPath }).toString();
+}
---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface ChangedFunction {
+    name: string;
+    filePath: string;
+    bodyBefore: string,
+    bodyAfter: string,
+    classHeader: string,
+}
+
+export interface ChangedText {
+    bodyBefore: string,
+    bodyAfter: string,
+    filePath: string,
+}
