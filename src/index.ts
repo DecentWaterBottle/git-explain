8c590246c77eb1ac3ab7e023218ad5e26703ab68
Jack Carter <jack.carter334@gmail.com>
2026-05-03 21:24:25 +0200

    Add change-type determination

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,14 @@ export function getChangedFiles(hash1: string, hash2: string, repoPath: string):
 export function getFileContent(fileName: string, hash1: string, repoPath: string): string {
     return execSync(`git show ${hash1}:${fileName}`, { cwd: repoPath }).toString();
 }
+
+export function getTypeOfChange(hash1: string, repoPath: string, fileName?: string): 'added' | 'deleted' | 'modified' | 'renamed' {
+    const fileArg = fileName ? `-- ${fileName}` : '';
+    const output = execSync(`git diff --name-status ${hash1}~1 ${hash1} ${fileArg}`, { cwd: repoPath }).toString();
+    const status = output.trim().split("\t")[0];
+    if (status === 'A') return 'added';
+    if (status === 'D') return 'deleted';
+    if (status === 'R') return 'renamed';
+    if (status === 'M') return 'modified';
+    throw new Error(`Unknown git status: ${status}`);
+}
